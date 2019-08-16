fdba1dc1720091600de70c72f004da93056da121
vasunangalia <vasunangalia789@gmail.com>
2019-08-16 06:35:30 +0530

    heroku

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const path = require("path");
 
 const app = express();
 
@@ -11,14 +12,22 @@ connectDB();
 
 app.use(express.json({ entended: false }));
 
-app.get("/", (req, res) => res.send("api running"));
-
 // define routes
 
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/posts", require("./routes/api/posts"));
 app.use("/api/profile", require("./routes/api/profile"));
+
+// serve static prod
+if (process.env.NODE_ENV === "production") {
+  // static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (requestAnimationFrame, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
